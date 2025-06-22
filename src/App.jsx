8cd378e5c9e0925723d717a47e4433f3faface4e
vanshkallra <vanshkalra.pro@gmail.com>
2025-06-22 07:18:21 +0530

    AppGrid naming error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import Navbar from './components/blueprint/Navbar';
-import AppGrid from './components/Appgrid';
+import AppGrid from './components/AppGrid';
 import WindowManager from './components/WindowManager';
 import './assets/css/blueprint/app.css';
 import './assets/css/blueprint/window.css';
